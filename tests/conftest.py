1d3c364717a6e778521ae1c538a181f6315464e7
u-gajera <gajera.uday.1@gmail.com>
2025-05-23 18:34:14 +0200

    adding test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,24 @@
-import importlib.metadata
+import pytest
+import nomad
+from nomad.config.models.config import Plugins
 
-import nomad.config
+# import your plugin entry points
+from nomad_battery_database import battery_app
+from nomad_battery_database.parsers import battery_parser
+from nomad_battery_database.schema_packages.battery_schema import m_package
 
-# nomad config is set up
-if getattr(nomad.config, "plugins", None) is None:
-    print("initializing nomad plugins manually...")
-    nomad.config.plugins = nomad.config.models.config.Plugins() 
-
-    # to register installed entry points
-    for entry_point in importlib.metadata.entry_points().select(group="nomad.plugin"):
-        nomad.config.plugins.entry_points.options[entry_point.name] = entry_point
-
-    print("nomad plugins have manually registered.")
-else:
-    try:
-        nomad.config.plugins.load_plugins()
-    except AttributeError:
-        print("warning:nomad plugin loading failed. Continuing with default settings.")
+@pytest.fixture(autouse=True)
+def init_nomad_plugins():
+    """
+    Initialize nomad.config.plugins with your battery-database plugin entry points
+    so that pytest can discover and use them without ValidationError.
+    """
+    nomad.config.plugins = Plugins(
+        entry_points=[
+            battery_app,
+            battery_parser,
+            m_package,
+        ],
+        plugin_packages=["nomad_battery_database"],
+    )
+    yield
